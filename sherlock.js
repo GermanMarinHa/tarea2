66c962f1d494eced1b50a51a1d74fce6ed8babe1
GermanMarinHa <germh_756@hotmail.com>
2015-09-02 14:20:21 -0500

    d

--- sherlock.js
+++ sherlock.js
@@ -1,6 +1,6 @@
 //Input Example
 
-_INPUT = "2\n3 9\n17 24";
+_INPUT = "3\n1 10\n1 100\n1 100";
 
 //Expected Output
 /*
@@ -22,3 +22,51 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+
+var separador = _INPUT.split("\n"); //separo la variable por cada \n
+//console.log (separador[0]); 
+//separador = arreglo_separado
+
+
+for (i=1; i<=separador[0]; i++) 
+     {
+
+   {var arreglo_separado = separador[i].split(" ");} //separo cada lugar en el arreglo entre 2
+            
+           var numeros_cuadrados=0; //se da valor a la cantidad de numeros cuadrados perfectos que hay en el rango
+            
+            inicio=parseInt(arreglo_separado[0]); //se da el primer valor del arreglo dividido al inicio
+            limite=parseInt(arreglo_separado[1]); //se da el segundo valor del arreglo dividido al limite
+            //console.log (inicio);
+            //console.log (limite); 
+            
+            
+            {
+            for (j=inicio; j<=limite; j++) //se inicia el for en el primer numero del rango y se termina en el ultimo
+                {
+                    console.log (j)+1;
+                    raiz = Math.sqrt(j); //raiz cuadrada del numero j
+                    //console.log (raiz);
+                    cuadrado = Math.floor(raiz);//se trunca el numero
+                    perfecto= (cuadrado*cuadrado);
+                    
+                    if  (perfecto==j) //si el numero truncado elevado al cuadrado es igual al numero j entonces j si es cuadrado perfecto
+                    {
+                    numeros_cuadrados = (numeros_cuadrados+1); //como si es cuadrado perfecto se suma 1 a la cantidad de numeros
+                    
+                    }
+                    else 
+                    {
+                    numeros_cuadrados = (numeros_cuadrados); //como no es igual entonces la variable se mantiene
+                    
+                    }
+                }
+             console.log  (numeros_cuadrados); }
+                
+      }    
+            
+          
+             
+
+ 
+
